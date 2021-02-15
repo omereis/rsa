f6f36566d5e37c21a8a90e24cd0dcdba8b49c15b
Omer Eisenberg <omereis@yahoo.com>
2021-02-15 12:26:41 +0200

    Added TStringVector, ERPCommand red pitaya command enumeration

--- rsa_types.h
+++ rsa_types.h
@@ -2,11 +2,21 @@
 #define	_RSA_INC
 
 #include <queue>
+#include <vector>
 #include <string>
+
 using namespace std;
 
+//-----------------------------------------------------------------------------
 #ifndef TStringQueue
 typedef queue<string> TStringQueue;
 #endif // TStringQueue
+//-----------------------------------------------------------------------------
+#ifndef TStringVector
+typedef vector<string> TStringVector;
+#endif // TStringVector
+//-----------------------------------------------------------------------------
+
+enum ERPCommand {ERPC_SAMPLING, ERPC_ERPC_GET_SETUP, ERPC_SET_TRIGGER, ERPC_GET_SIGNALS, ERPC_GET_SAMPLES, ERPC_NONE};
 
 #endif // _RSA_INC
