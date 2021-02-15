3131cb5cfd92478bcb69ca303ce14aee0da6b27f
Omer Eisenberg <omereis@yahoo.com>
2021-02-15 08:15:17 +0200

    Added parsing basic class files

---
+++ Parse.cpp
@@ -0,0 +1,15 @@
+#ifndef	__PARSE_H_INC
+#define__PARSE_H_INC
+include "parse.h"
+
+//#define PORT 5500
+
+#include "comm.h"
+#include "rsa_types.h"
+
+class TCommParser {
+public:
+protected:
+private:
+};
+#endif // __PARSE_H_INC
---
+++ Parse.h
@@ -0,0 +1,13 @@
+#include "rapidjson/document.h"     // rapidjson's DOM-style API
+#include "rapidjson/prettywriter.h" // for stringify JSON
+/*
+#include <jsoncpp/json/json.h>
+#include <jsoncpp/json/reader.h>
+#include <jsoncpp/json/writer.h>
+#include <jsoncpp/json/value.h>
+*/
+
+//#define PORT 5500
+
+#include "comm.h"
+#include "rsa_types.h"
