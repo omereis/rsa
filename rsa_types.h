8d9b08d3c044c10c50745e4b6ea4c9caf316db44
Omer Eisenberg <omereis@yahoo.com>
2021-02-17 12:28:06 +0200

    Refind GET_TRIGGER command

--- rsa_types.h
+++ rsa_types.h
@@ -17,6 +17,6 @@ typedef vector<string> TStringVector;
 #endif // TStringVector
 //-----------------------------------------------------------------------------
 
-enum ERPCommand {ERPC_SAMPLING, ERPC_ERPC_GET_SETUP, ERPC_SET_TRIGGER, ERPC_GET_SIGNALS, ERPC_GET_SAMPLES, ERPC_NONE};
+enum ERPCommand {ERPC_SAMPLING, ERPC_GET_TRIGGER, ERPC_SET_TRIGGER, ERPC_GET_SIGNALS, ERPC_GET_SAMPLES, ERPC_NONE};
 
 #endif // _RSA_INC
