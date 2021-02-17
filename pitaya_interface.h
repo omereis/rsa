2bf493b29f46f26e8782aa197240f9fd23a6e228
Omer Eisenberg <omereis@yahoo.com>
2021-02-17 12:25:18 +0200

    FollowCommand returns bool

--- pitaya_interface.h
+++ pitaya_interface.h
@@ -8,11 +8,18 @@
 
 #include "rsa_types.h"
 //enum ERPCommand {ERPC_SAMPLING, ERPC_ERPC_GET_SETUP, ERPC_SET_TRIGGER, ERPC_GET_SIGNALS, ERPC_GET_SAMPLES, ERPC_NONE};
-
+using namespace rapidjson;
+using namespace std;
+//-----------------------------------------------------------------------------
 class TPitayaInterface { // Res Pitaya Commands
 public:
 	TPitayaInterface ();
+	TPitayaInterface (Document &document);
+	~TPitayaInterface ();
+	bool FollowCommand (const std::string &str, Document &docCommand, string &strReply);
 private:
+	Document m_document;
 	
 };
+//-----------------------------------------------------------------------------
 #endif // __RPCommand_H
