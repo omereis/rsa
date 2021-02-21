11149664145f9e8ec7d4b086a02ae97a31750919
Omer Eisenberg <omereis@yahoo.com>
2021-02-21 11:34:22 +0200

    comm.h, rsa_types.h include in rsa.h

--- comm.h
+++
@@ -1,9 +0,0 @@
-#ifndef	_COMM_INC
-#define	_COMM_INC
-
-#define	DEFAULT_PORT	5500
-#define	DEFAULT_ADDRESS	"127.0.0.1"
-
-#define	COMM_BUFFER_LENGTH	8192
-
-#endif
--- rsa.h
+++ rsa.h
@@ -1,11 +1,19 @@
 #ifndef	_RSA_INC
 #define	_RSA_INC
-
 #include <queue>
 #include <vector>
 #include <string>
 
 using namespace std;
+
+#define	DEFAULT_PORT	5500
+#define	DEFAULT_ADDRESS	"127.0.0.1"
+
+#define	COMM_BUFFER_LENGTH	8192
+
+#ifndef	PITAYA_MOCK_NAME
+#define	PITAYA_MOCK_NAME	"red_pitaya.json"
+#endif	//PITAYA_MOCK_NAME
 
 //-----------------------------------------------------------------------------
 #ifndef TStringQueue
@@ -18,5 +26,4 @@ typedef vector<string> TStringVector;
 //-----------------------------------------------------------------------------
 
 enum ERPCommand {ERPC_SAMPLING, ERPC_GET_TRIGGER, ERPC_SET_TRIGGER, ERPC_GET_SIGNALS, ERPC_GET_SAMPLES, ERPC_NONE};
-
-#endif // _RSA_INC
+#endif // RSA
