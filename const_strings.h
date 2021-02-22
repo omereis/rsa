68287fbcafddc05f64f2e33f6ac040eec7e43f2b
Omer Eisenberg <omereis@yahoo.com>
2021-02-22 08:20:42 +0200

    JSON commands definitions

---
+++ const_strings.h
@@ -0,0 +1,18 @@
+#ifndef	__CONST_STRINGS_H_INC
+#define	__CONST_STRINGS_H_INC
+
+#include <string>
+
+using namespace std;
+
+extern const std::string g_strTrigger;
+extern const std::string g_strLevel;
+
+#define	RPSU_TRIGGER	"trigger" // Red Pitaya Set Up
+#define	RPSU_LEVEL		"level"
+#define	RPSU_DELAY		"delay"
+#define	RPSU_SOURCE		"source"
+#define	RPSU_DIR		"direction"
+
+//-----------------------------------------------------------------------------
+#endif //__CONST_STRINGS
