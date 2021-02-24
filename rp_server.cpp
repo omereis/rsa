db46ef7b80ce75367ed998101fb5f53063b0b46f
Omer Eisenberg <omereis@yahoo.com>
2021-02-24 11:54:35 +0200

    added sampler, that saves samples to a queue, sending reply samples to client

--- rp_server.cpp
+++ rp_server.cpp
@@ -1,11 +1,15 @@
 // Server side C/C++ program to demonstrate Socket programming 
 #include <mutex>
+#include <thread>
+#include <chrono>
 
+#include<iostream>
 #include <unistd.h> 
 #include <stdio.h> 
 #include <sys/socket.h> 
 #include <stdlib.h> 
 #include <netinet/in.h> 
+#include <arpa/inet.h>
 #include <string.h> 
 
 #include "rapidjson/document.h"     // rapidjson's DOM-style API
@@ -14,7 +18,7 @@
 #include "pitaya_interface.h"
 
 #include "rsa.h"
-//#include "rsa_types.h"
+#include "misc.h"
 
 using namespace rapidjson;
 using namespace std;
@@ -30,6 +34,7 @@ int GetCliPort (int argc, char const *argv[])
 		nPort = DEFAULT_PORT;
 	return (nPort);
 }
+/*
 //-----------------------------------------------------------------------------
 
 int OpenSocket (int nPort)
@@ -64,28 +69,17 @@ int OpenSocket (int nPort)
 	}
 	return (nServerFd);
 }
+*/
 //-----------------------------------------------------------------------------
 
-bool ActOnCommand (const std::string &strJson, Document &docCommand, string &strReply)
+bool ActOnCommand (const std::string &strJson, Document &docCommand, const char *szClientIP, string &strReply)
 {
 	bool f;
 
 	try {
 		assert(docCommand.IsObject());
-		TPitayaInterface pi(docCommand);
-		//f = pi.FollowCommand (strJson, strReply);
+		TPitayaInterface pi(szClientIP);
 		f = pi.FollowCommand (docCommand, strReply);
-		//f = true;
-/*
-		if (document.HasMember("command")) {
-			printf ("'command' found\n");
-			const Value &val = document["command"];
-			if (val.IsString())
-				printf ("Command value is '%s'\n", val.GetString());
-			else
-				printf ("Command is not a string\n");
-		}
-*/
 	}
 	catch (exception &e) {
 		printf ("ActOnCommand Runtime error:\n%s\n", e.what());
@@ -103,6 +97,63 @@ string ToLower (const std::string &str)
 		strLower += tolower(str[n]);
 	return (strLower);
 }
+//-----------------------------------------------------------------------------
+int SampleSignal (float *afSignals, int nLength)
+{
+	FILE *file = fopen ("signal.dat", "r");
+	char szBuf[100], *sz;
+	int n=0;
+	float f;
+	
+	if (file != NULL) {
+		while (fgets(szBuf, 100, file) != NULL) {
+			afSignals[n++] = atof(szBuf);
+			if (n >= nLength)
+				break;
+		}
+	}
+	fclose (file);
+	return (n);
+}
+//-----------------------------------------------------------------------------
+#include "rp_signal.h"
+std::mutex mutexQueue;
+queue<TRpSignal> qSignals;
+queue<time_t> qMine;
+std::mutex mutexStop;
+bool g_fStop;
+//-----------------------------------------------------------------------------
+using namespace std::chrono_literals;
+void SampleInput ()
+{
+	bool fContinue=true;
+	float *afSignals = new float[MAX_BUFF_SIZE];
+	int nSamples;
+
+	while (fContinue) {
+		if ((nSamples = SampleSignal (afSignals, MAX_BUFF_SIZE)) > 0) {
+			TRpSignal signal (afSignals, nSamples);
+			mutexQueue.lock();
+			while (qSignals.size() >= MAX_QUEUE_LENGTH)
+				qSignals.pop();
+			qSignals.push (TRpSignal(signal));
+			mutexQueue.unlock();
+		}
+
+		std::this_thread::sleep_for(2000ms);
+
+		mutexStop.lock();
+		fContinue = !g_fStop;
+		mutexStop.unlock();
+	}
+	delete[] afSignals;
+}
+/*
+sampler - read signals from input(file) & put in signals queue
+psd - perform PSD analysis
+mca - perform MCA analysis
+*/
+//-----------------------------------------------------------------------------
 
 int main(int argc, char const *argv[])
 {
@@ -114,14 +165,16 @@ int main(int argc, char const *argv[])
 	int nPort;// = DEFAULT_PORT;
 	TStringQueue qCommand, qReply;
 	std::mutex mutex;
+	std::thread threadSample (SampleInput);
 	string strReply;
 	Document document;  // Default template parameter uses UTF8 and MemoryPoolAllocator.
 
 	nPort = GetCliPort (argc, argv);
-	server_fd = OpenSocket (nPort);
+	server_fd = OpenServerSocket (nPort);
 	char szHello[1024];
 
 	int n=1;
+	g_fStop = false;
 	do {
 		fprintf (stderr, "Waiting at port %d\n", nPort);
 		if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0){ 
@@ -129,15 +182,17 @@ int main(int argc, char const *argv[])
 			exit(EXIT_FAILURE);
 		}
 		memset (buffer, 0, COMM_BUFFER_LENGTH);
-		valread = read( new_socket , buffer, COMM_BUFFER_LENGTH);
+		valread = read(new_socket, buffer, COMM_BUFFER_LENGTH);
+		struct in_addr ipAddr = address.sin_addr;
+		char szClientIP[INET_ADDRSTRLEN];
+		inet_ntop(AF_INET, &ipAddr, szClientIP, INET_ADDRSTRLEN);
+		printf ("Client address is %s\n", szClientIP);
 		try {
 			string strMessage (buffer);
 			string strJson = ToLower (strMessage);
-			//if ((fParseError = document.Parse(buffer).HasParseError()) == false)
 			if ((fParseError = document.Parse(strJson.c_str()).HasParseError()) == false)
-				if (!ActOnCommand (strJson, document, strReply))
+				if (!ActOnCommand (strJson, document, szClientIP, strReply))
 					valread = 0;
-			//fParse = reader.parse (buffer, root);
 		}
 		catch (std::exception &e) {
 			fprintf (stderr, "Parsing error:\n%s\n", e.what());
@@ -149,13 +204,21 @@ int main(int argc, char const *argv[])
 		printf ("New Message of %d bytes\n", valread);
 		printf ("JSON Parsing: %s\n", strReply.c_str());
 		printf ("-------------------------------------------------------------------------------\n");
-		//printf("%s\n", buffer);
-		//printf ("-------------------------------------------------------------------------------\n");
-		//printf("Message recieved: %s, valread=%d\n", buffer, valread);
-		//sprintf (szHello, "Server Hello #%d", n++);
 		send (new_socket , strReply.c_str(), strReply.size() ,0);
-		//printf("Hello message sent\n");
+		for (int n=0 ; n < 10 ; n++) {
+			mutexQueue.lock();
+			if (!qMine.empty()) {
+				cout << qMine.front() << "\n";
+				qMine.pop();
+			}
+			mutexQueue.unlock();
+		}
 	} while (valread > 0);
+	g_fStop = true;
+	threadSample.join();
+	//mutexStop.lock();
+	//g_fStop = true;
+	//mutexStop.unlock();
 	return 0;
 }
 
