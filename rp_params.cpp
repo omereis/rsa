38007ec2c078673f734f088e417bb1be960c78a8
Omer Eisenberg <omereis@yahoo.com>
2021-03-04 09:26:56 +0200

    added JSON,string access to sampling parameters, trigger

--- rp_params.cpp
+++ rp_params.cpp
@@ -1,4 +1,4 @@
-/******************************************************************************\
+;;/******************************************************************************\
 |                                rp_params.cpp                                 |
 \******************************************************************************/
 #include "rp_params.h"
@@ -14,77 +14,16 @@ TRedPitayaParams::TRedPitayaParams ()
 bool TRedPitayaParams::Clear ()
 {
 	m_trigger.Clear ();
+	m_rate       = RP_SMP_125M;
+	m_channel    = RP_CH_1;
+	m_decimation = RP_DEC_1;
 }
 //-----------------------------------------------------------------------------
 
 bool TRedPitayaParams::LoadFromJson (const std::string &strJson)
 {
 	return (true);
-/*
-	bool fLoad;
-
-	if ((fLoad = LoadFromJson (PITAYA_MOCK_NAME)) == true) {
-	}
-	return (fLoad);
-*/
 }
-
-/*
-bool TRedPitayaParams::LoadFromJson (const std::string &strJson)
-{
-	bool fLoad;
-	Document docSetup;
-
-	if ((fLoad = ReadPitayaSetup (docSetup)) == true)
-		if (docSetup.HasMember ("trigger"))
-			fLoad = m_trigger.LoadFromJson (docSetup["trigger"]);
-}
-*/
-//-----------------------------------------------------------------------------
-
-/*
-bool TRedPitayaParams::GetTrigger (Document &docTrigger)
-{
-	return (m_trigger.JsonTrigger (docTrigger));
-}
-*/
-//-----------------------------------------------------------------------------
-
-/*
-bool TRedPitayaParams::GetSetup (const Value &valSetup, std::string &strReply)
-{
-	bool f;
-
-	try {
-		const char *szCommand = valSetup.GetString();
-		if (strcmp (szCommand, RPSU_GET_TRIGGER) == 0) {
-			Document docTrigger;
-			docTrigger.SetObject();
-			m_trigger.JsonTrigger (docTrigger);
-			**
-			std::string strLevel ("level");
-
-			docTrigger.SetObject();
-			Value val (strLevel.c_str(), strLevel.size(), docTrigger.GetAllocator());
-			docTrigger.AddMember (val, 1e-5, docTrigger.GetAllocator());
-			**
-
-			StringBuffer sb;
-			Writer<StringBuffer> writer(sb);
-			docTrigger.Accept (writer);
-			const char *szReply = sb.GetString();
-			printf ("%s\n", szReply);
-			strReply = std::string (sb.GetString());
-		}
-		f = true;
-	}
-	catch (std::exception &e) {
-		f = false;
-		strReply = e.what();
-	}
-	return (f);
-}
-*/
 //-----------------------------------------------------------------------------
 
 void TRedPitayaParams::GetTrigger (Json::Value &jvalSetup)
@@ -107,3 +46,130 @@ bool TRedPitayaParams::SetTrigger (const Json::Value &jsonTrigger, std::string &
 	}
 	return (f);
 }
+//-----------------------------------------------------------------------------
+
+rp_acq_sampling_rate_t TRedPitayaParams::GetRate() const
+{
+	return (m_rate);
+}
+//-----------------------------------------------------------------------------
+
+void TRedPitayaParams::SetRate (rp_acq_sampling_rate_t rate)
+{
+	m_rate = rate;
+}
+
+//-----------------------------------------------------------------------------
+rp_channel_t TRedPitayaParams::GetChannel () const
+{
+	return (m_channel);
+}
+
+//-----------------------------------------------------------------------------
+void TRedPitayaParams::SetChannel (rp_channel_t channel)
+{
+	m_channel = channel;
+}
+//-----------------------------------------------------------------------------
+
+rp_acq_decimation_t TRedPitayaParams::GetDecimation () const
+{
+	return (m_decimation);
+}
+//-----------------------------------------------------------------------------
+
+void TRedPitayaParams::GetDecimation (rp_acq_decimation_t decimation)
+{
+	m_decimation = decimation;
+}
+//-----------------------------------------------------------------------------
+
+/*
+string  TRedPitayaParams::GetDecimationName () const
+{
+	return (GetDecimationName (m_decimation));
+}
+*/
+//-----------------------------------------------------------------------------
+
+string TRedPitayaParams::GetRateName () const
+{
+	return (SamplingRateName (GetRate()));
+}
+//-----------------------------------------------------------------------------
+
+void TRedPitayaParams::SetRate (const string &strRate)
+{
+	SetRate(SamplingRateFromName(strRate));
+}
+//-----------------------------------------------------------------------------
+
+string TRedPitayaParams::GetChannelName () const
+{
+	return (::GetChannelName (m_channel));
+}
+//-----------------------------------------------------------------------------
+void TRedPitayaParams::SetChannel (const string &strChannel)
+{
+	m_channel = SetChannelFromName (strChannel);
+}
+//-----------------------------------------------------------------------------
+
+bool TRedPitayaParams::GetSamplingParams (std::string &strReply)
+{
+	bool f;
+	Json::Value jParams;
+	
+	try {
+		strReply = "";
+		if ((f = GetParams (jParams)) == true)
+			strReply = StringifyJson (jParams);
+		f = true;
+	}
+	catch (std::exception &e) {
+		f = false;
+	}
+	return (f);
+}
+//-----------------------------------------------------------------------------
+
+bool TRedPitayaParams::GetTriggerAsString (std::string &strReply) const
+{
+	bool f;
+
+	try {
+		strReply = StringifyJson(m_trigger.AsJson ());
+		f = true;
+	}
+	catch (std::exception &e) {
+		strReply = e.what();
+		f = false;
+	}
+	return (f);
+}
+//-----------------------------------------------------------------------------
+
+bool TRedPitayaParams::GetParams (Json::Value &jParams) const
+{
+	bool f;
+
+	try {
+		jParams[RPSU_RATE] = GetRateName ();
+		jParams[RPSU_CHANNEL] = GetChannelName ();
+		jParams[RPSU_DECIMATION] = GetDecimationName (m_decimation);
+		jParams[RPSU_TRIGGER] = m_trigger.AsJson ();
+		f = true;
+	}
+	catch (std::exception ) {
+		f = false;
+	}
+	return (f);
+}
+//-----------------------------------------------------------------------------
+
+bool TRedPitayaParams::SetSamplingParams (const Json::Value &jParams, std::string &strReply)
+{
+	strReply = "TRedPitayaParams::SetSamplingParams";
+	return (true);
+}
+//-----------------------------------------------------------------------------
--- rp_params.h
+++ rp_params.h
@@ -5,9 +5,12 @@
 #ifndef	__RP_PARAMS_H
 #define	__RP_PARAMS_H
 
+#include <string>
 #include "misc.h"
 #include "rp_trigger.h"
 #include "jsoncpp/json/json.h"
+
+using namespace std;
 
 class TRedPitayaParams {
 public:
@@ -16,12 +19,30 @@ public:
 	bool Clear ();
 	bool LoadFromJson (const std::string &strJson);
 
-	//bool GetTrigger (Document &docTrigger);
-	//bool GetSetup (const Value &valSetup, std::string &strReply);
 	void GetTrigger (Json::Value &jvalSetup);
 	bool SetTrigger (const Json::Value &jsonTrigger, std::string &strReply);
+	rp_acq_sampling_rate_t GetRate() const;
+	void SetRate (rp_acq_sampling_rate_t rate);
+	rp_channel_t GetChannel () const;
+	void SetChannel (rp_channel_t channel);
+	rp_acq_decimation_t GetDecimation () const;
+	void GetDecimation (rp_acq_decimation_t decimation);
+
+	string GetRateName () const;
+	void SetRate (const string &strRate);
+	string GetChannelName () const;
+	void SetChannel (const string &strChannel);
+
+	bool GetSamplingParams (std::string &strReply);
+	bool SetSamplingParams (const Json::Value &jParams, std::string &strReply);
+
+	bool GetTriggerAsString (std::string &strReply) const;
 protected:
+	bool GetParams (Json::Value &jParams) const;
 private:
 	TRedPitayaTrigger m_trigger;
+	rp_acq_sampling_rate_t m_rate;
+	rp_channel_t m_channel;
+	rp_acq_decimation_t m_decimation;
 };
 #endif // __RP_PARAMS_H
